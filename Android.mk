7f13462979c3d845fcd59d5e81110ced67d0f033
U-yuifan-PC\yuifan <yuifan@yuifan-PC.(none)>
2013-05-14 22:50:22 -0700

    update 4.2

--- Android.mk
+++ Android.mk
@@ -16,8 +16,11 @@ LOCAL_ARM_MODE := arm
 LOCAL_SRC_FILES:= \
 	src/base/ftbbox.c \
 	src/base/ftbitmap.c \
+	src/base/ftfstype.c \
 	src/base/ftglyph.c \
+	src/base/ftlcdfil.c \
 	src/base/ftstroke.c \
+	src/base/fttype1.c \
 	src/base/ftxf86.c \
 	src/base/ftbase.c \
 	src/base/ftsystem.c \
@@ -40,15 +43,6 @@ LOCAL_CFLAGS += -W -Wall
 LOCAL_CFLAGS += -fPIC -DPIC
 LOCAL_CFLAGS += "-DDARWIN_NO_CARBON"
 LOCAL_CFLAGS += "-DFT2_BUILD_LIBRARY"
-
-# enable the FreeType internal memory debugger in the simulator
-# you need to define the FT2_DEBUG_MEMORY environment variable
-# when running the program to activate it. It will dump memory
-# statistics when FT_Done_FreeType is called
-#
-ifeq ($(TARGET_SIMULATOR),true)
-LOCAL_CFLAGS += "-DFT_DEBUG_MEMORY"
-endif
 
 # the following is for testing only, and should not be used in final builds
 # of the product
